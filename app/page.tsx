d91284556d1fadd954c5307b4e55fdeea9112063
bloodyrainday <bloodyrainday@gmail.com>
2025-06-08 16:15:01 +0300

    add Filters component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, Categories, SortPopup } from "@/components/shared";
+import { Container, Title, Filters } from "@/components/shared";
 import { TopBar } from "@/components/shared/top-bar";
 
 export default function Home() {
@@ -6,8 +6,20 @@ export default function Home() {
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
 
-        <TopBar />
+      <Container className="pb-14">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          <div className="flex flex-col gap-16">
+            {/* <ProductsGroupList title="Пиццы" items={[1, 2, 3, 4, 5]} />
+            <ProductsGroupList title="Комбо" items={[1, 2, 3, 4, 5]} /> */}
+          </div>
+        </div>
       </Container>
     </>
   );
