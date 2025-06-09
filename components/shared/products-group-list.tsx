433546fde82713c6caa2e2c9d2a3b737ba6cb8ba
bloodyrainday <bloodyrainday@gmail.com>
2025-06-09 12:09:23 +0300

    create setActiveCategoryId and apply useCategoryStore custom hook

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -4,6 +4,7 @@ import { Title } from "./title";
 import { ProductCard } from "./product-card";
 import { cn } from "@/lib/utils";
 import { useIntersection } from "react-use";
+import { useCategoryStore } from "@/store/category";
 
 interface Props {
   title: string;
@@ -20,6 +21,7 @@ export const ProductsGroupList: React.FC<Props> = ({
   categoryId,
   listClassName,
 }) => {
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
   const intersectionRef = React.useRef<any>(null);
   const intersection = useIntersection(intersectionRef, {
     threshold: 0.4,
@@ -28,6 +30,7 @@ export const ProductsGroupList: React.FC<Props> = ({
   React.useEffect(() => {
     if (intersection?.isIntersecting) {
       console.log(categoryId, title);
+      setActiveCategoryId(categoryId);
     }
   }, [intersection?.isIntersecting]);
 
