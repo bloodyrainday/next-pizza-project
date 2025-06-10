ff030856aaffdcdce01850495bb771647043b2ed
bloodyrainday <bloodyrainday@gmail.com>
2025-06-10 13:24:44 +0300

    create seed.ts file and up function in it

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -8,6 +8,7 @@ export async function GET() {
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
+
   const user = await prisma.user.create({
     data,
   });
---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import { prisma } from "./prisma-client";
+import { hashSync } from "bcrypt";
+
+async function up() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: "User",
+        email: "user@test.com",
+        password: hashSync("1111", 10),
+        verified: new Date(),
+        role: "USER",
+      },
+      {
+        fullName: "Admin",
+        email: "admin@test.com",
+        password: hashSync("1111", 10),
+        verified: new Date(),
+        role: "ADMIN",
+      },
+    ],
+  });
+}
+
+async function down() {}
+
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (e) {
+    console.log(e);
+  }
+}
