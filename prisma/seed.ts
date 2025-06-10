2764f60885d2448c1949d4fa5114b37b61da1032
bloodyrainday <bloodyrainday@gmail.com>
2025-06-10 16:41:33 +0300

    add pizza1, pizza2, pizza3 data and randomDecimalNumber

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,10 @@ import { categories, ingredients, products } from "./constants";
 import { prisma } from "./prisma-client";
 import { hashSync } from "bcrypt";
 
+const randomDecimalNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) * 10 + min * 10) / 10;
+};
+
 async function up() {
   await prisma.user.createMany({
     data: [
@@ -32,6 +36,46 @@ async function up() {
 
   await prisma.product.createMany({
     data: products,
+  });
+
+  const pizza1 = await prisma.product.create({
+    data: {
+      name: "Пепперони фреш",
+      imageUrl:
+        "https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp",
+      categoryId: 1,
+      ingredients: {
+        connect: ingredients.slice(0, 5),
+      },
+    },
+  });
+
+  const pizza2 = await prisma.product.create({
+    data: {
+      name: "Сырная",
+      imageUrl:
+        "https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp",
+      categoryId: 1,
+      ingredients: {
+        connect: ingredients.slice(5, 10),
+      },
+    },
+  });
+
+  const pizza3 = await prisma.product.create({
+    data: {
+      name: "Чоризо фреш",
+      imageUrl:
+        "https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp",
+      categoryId: 1,
+      ingredients: {
+        connect: ingredients.slice(10, 40),
+      },
+    },
+  });
+
+  await prisma.productItem.createMany({
+    data: [{}],
   });
 }
 
